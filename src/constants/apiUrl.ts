3b6ee74c1abf7786fb4dfd1278da2c0bdfbdbe95
den <066den@gmail.com>
2022-06-24 09:29:20 +0300

    reload

--- src/constants/apiUrl.ts
+++ src/constants/apiUrl.ts
@@ -2,7 +2,7 @@ export const apiUrl = {
 	BASE_API_URL: 'https://api.openweathermap.org/data/',
 	WEATHER_API_URL: '3.0/onecall',
 	WEATHER_SHORT_API_URL: '2.5/weather',
-	CITY_API_URL: 'http://api.openweathermap.org/geo/1.0/direct',
+	CITY_API_URL: 'https://api.openweathermap.org/geo/1.0/direct',
 	FLAG_API_URL: 'https://openweathermap.org/images/flags/',
 	API_KEY: '3553226ab36e9a937d8a972d103239e9',
 };
